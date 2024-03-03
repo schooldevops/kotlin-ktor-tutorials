7c807a695886cb53e41a5dff0dbcb82590ece89d
kido <baekido@gmail.com>
2024-03-03 23:14:23 +0900

    add ktor websocket

---
+++ 01.KtorServer/01.GettingStartedWithKtorServer/samples/05.ktor-chat/src/main/kotlin/com/example/plugins/Sockets.kt
@@ -0,0 +1,47 @@
+package com.example.plugins
+
+import com.example.Connection
+import io.ktor.network.sockets.*
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import java.time.Duration
+import java.util.*
+import kotlin.collections.LinkedHashSet
+
+fun Application.configureSockets() {
+    val connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
+
+    install(WebSockets) {
+        pingPeriod = Duration.ofSeconds(15)
+        timeout = Duration.ofSeconds(15)
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+    }
+    routing {
+        webSocket("/chat") {
+            println("Adding user!")
+            val thisConnection = Connection(this)
+            connections += thisConnection
+
+            try {
+                send("You are connected!")
+                for(frame in incoming) {
+                    frame as? Frame.Text ?: continue
+                    val receivedText = frame.readText()
+                    val textWithUsername = "[${thisConnection.name}]: $receivedText"
+                    connections.forEach{
+                        it.session.send(textWithUsername)
+                    }
+
+                }
+            } catch (e: Exception) {
+                println(e.localizedMessage)
+            } finally {
+                println("Removing $thisConnection")
+                connections -= thisConnection
+            }
+        }
+    }
+}
